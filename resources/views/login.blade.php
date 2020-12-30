2383379cb5352a338adb3acc7ca5a67312c017b2
UDITHA J <udithaj.98@gmail.com>
2020-12-30 21:55:03 +0530

    AdminController and login route created

---
+++ resources/views/login.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+
+<form>
+    <input type = "text" name = "username" id = "username" placeholder="Username"><br>
+    <input type = "password" name = "password" id ="password" placeholder="Password"><br>
+    <input type="submit">
+</form>
+    
+</body>
+</html>
